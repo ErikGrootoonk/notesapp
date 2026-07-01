a89bbf692f2f0554ba77ef5c922ff7260b677bd9
Erik Grootoonk <erik.grootoonk@pinkroccade.nl>
2026-07-01 16:43:51 +0200

    serverport to 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 const NOTES_DIR = path.resolve(__dirname, '../notes');
 
 app.use(express.json());
